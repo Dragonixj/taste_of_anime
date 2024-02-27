fac9c03ae36f180d87374d75c21c47ef9e9373af
Dragonixj <delafiado@gmail.com>
2024-02-27 09:54:05 +0000

    adding the firebase sdk and config files

---
+++ client/src/components/Auth/Login.js
@@ -0,0 +1,10 @@
+import firebase from 'firebase/app';
+import 'firebase/auth';
+import { useState } from 'react';
+import firebaseConfig from '../../../../firebase/firebaseConfig';
+
+firebase.initializeApp(firebaseConfig);
+
+const Login = () => {
+    const [email, setEmail] = useState('');
+};
