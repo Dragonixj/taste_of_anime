9f0444442239cefbdbe5d5a034020244c85bc010
delafiado@gmail.com <delafiado@gmail.com>
2024-02-29 16:19:22 +0000

    working on a basic structure now

--- client/src/components/Home.jsx
+++ client/src/components/Home.jsx
@@ -1,14 +1,15 @@
 import React from "react";
+import Intro from "./Intro";
 import NavBar from "./NavBar";
 import Spotlight from "./Spotlight";
 
 
 const Home = () => {
 
-
     return (
         <div>
             <NavBar />
+            <Intro />
             <Spotlight />
         </div>
     )
--- client/src/components/NavBar.jsx
+++ client/src/components/NavBar.jsx
@@ -1,8 +1,11 @@
+import React from "react";
+import Nav from "react-bootstrap/Nav";
+import Navbar from "react-bootstrap/Navbar";
+
 const NavBar = () => {
     return (
-        <div>
-            -------NavBar Component Goes Here--------
-        </div>
+        <NavBar>
+        </NavBar>
     )
 }
 
