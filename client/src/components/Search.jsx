95dd5df8db1865578499a5c0eb273509e3cb9f52
delafiado@gmail.com <delafiado@gmail.com>
2024-03-08 15:20:37 +0000

    working on the search functionality

---
+++ client/src/components/Search.jsx
@@ -0,0 +1,17 @@
+import React, { useState } from 'react';
+import { FaSearch } from 'react-icons/fa';
+
+function Search() {
+    return (
+        <form>
+            <input type="text" />
+        </form>
+    );
+}
+
+const FormStyle = styled.form`
+    margin: 0rem 20rem;
+    position: relative;
+`;
+
+export default Search;
