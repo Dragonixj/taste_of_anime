ee2f8a29b5d8d54f1e296b4092f8931c92aff419
delafiado@gmail.com <delafiado@gmail.com>
2024-03-14 19:10:44 +0000

    almost done with the frontend

--- client/src/components/Spotlight.jsx
+++ client/src/components/Spotlight.jsx
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from 'react';
 import { styled } from 'styled-components';
 import { Splide, SplideSlide } from '@splidejs/react-splide';
 import '@splidejs/react-splide/css';
+import { Link } from 'react-router-dom';
 
 function Spotlight() {
     const [popular, setPopular] = useState([]);
@@ -39,9 +40,11 @@ function Spotlight() {
                         return (
                             <SplideSlide key={recipe.id}>
                                 <Card>
-                                    <p>{recipe.title}</p>
-                                    <img src={recipe.image} alt={recipe.title} />
-                                    <Gradient />
+                                    <Link to={'/recipe/' + recipe.id}>
+                                        <p>{recipe.title}</p>
+                                        <img src={recipe.image} alt={recipe.title} />
+                                        <Gradient />
+                                    </Link>
                                 </Card>
                             </SplideSlide>
                         );
