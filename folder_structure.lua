0c21781ed8e16466c3da2180313f4c0a64afe040
delafiado@gmail.com <delafiado@gmail.com>
2024-03-14 11:08:02 +0000

    made some changes

--- folder_structure.lua
+++ folder_structure.lua
@@ -1,5 +1,5 @@
-taste_of_anime
-├── backend
+taste of anime
+├── backend -- backend directory
 │   ├── app
 │   │   ├── auth
 │   │   │   └── auth_routes.py
@@ -26,46 +26,66 @@ taste_of_anime
 │       ├── App.js
 │       ├── components
 │       │   ├── Auth
-│       │   │   ├── Login.js -- login component
-│       │   │   └── Signup.js -- Signup component
+│       │   │   ├── Login.js
+│       │   │   └── Signup.js
+│       │   ├── Category.jsx
 │       │   ├── Intro.jsx
-│       │   ├── Likes.jsx -- like component for working with likecount
-│       │   ├── NavBar.jsx -- navbar component
+│       │   ├── Likes.jsx
+│       │   ├── NavBar.jsx
 │       │   ├── pages
-│       │   │   ├── Home.jsx -- homepage
-│       │   │   └── Pages.jsx -- pages component mainly for routing the pages
+│       │   │   ├── Cuisine.jsx -- individual cuisine type recipes
+│       │   │   ├── Home.jsx    -- Home page for the web application
+│       │   │   └── Pages.jsx   -- for routing
 │       │   ├── Recipes.jsx
+│       │   ├── Search.jsx
 │       │   ├── Spotlight.jsx
 │       │   ├── styles
 │       │   │   ├── intro.css
 │       │   │   └── navbar.css
 │       │   └── UserPage.jsx
-│       ├── firebaseConfig.js
 │       ├── index.css
 │       ├── index.js
 │       ├── reportWebVitals.js
 │       └── setupTests.js
-├── folder_structure.txt
+├── folder_structure.lua
 ├── frontend
-│   ├── anime-index.html
 │   ├── Anime-logo
 │   │   ├── Anime-logo.jpg
 │   │   ├── back-ground.jpg
-│   │   └── chef-logo.jpg
+│   │   ├── chef-logo.jpg
+│   │   └── video
+│   │       ├── video (1080p).mp4
+│   │       ├── video1.mp4
+│   │       ├── video2.mp4
+│   │       └── video3.mp4
 │   ├── appetizer-recipe
+│   │   ├── Baileys Cookies and Cream Parfaits.jpg
 │   │   ├── Burrata Bruschetta.jpg
+│   │   ├── Chocolate Strawberry Cake.jpg
 │   │   ├── Garlic Herb Roasted Potatoes Carrots and Zucchini.jpg
+│   │   ├── Grilled Chicken Skewers With Spicy Pesto _ Italian Food Forever.jpg
+│   │   ├── healthyeats-local.jpg
 │   │   ├── image-recipe
 │   │   │   ├── French onion sausage rolls.jpg
 │   │   │   └── recipe.jpg
+│   │   ├── maincorse-italian.jpg
+│   │   ├── ORANGE CHICKEN.jpg
 │   │   ├── Paleo Tailgating Recipes.jpg
+│   │   ├── Pan Seared Salmon with Beurre Blanc Sauce.jpg
 │   │   ├── recipe.jpg
 │   │   ├── Shrimp Recipes.jpg
 │   │   ├── Smoked Salmon Crostini .jpg
-│   │   └── Teriyaki chicken pineapple kabobs.jpg
+│   │   ├── Sweet and Spicy Baked Popcorn Chicken.jpg
+│   │   ├── Teriyaki chicken pineapple kabobs.jpg
+│   │   └── This Perfect Cheesecake Will Make You Feel Like a Pro Baker.jpg
 │   ├── image-back
-│   │   └── background.jpg
-│   ├── index-style.css
+│   │   ├── background-2.jpg
+│   │   ├── background.jpg
+│   │   └── Baileys Cookies and Cream Parfaits.jpg
+│   ├── login.html
+│   ├── login-style.css
+│   ├── main-index.html
+│   ├── main-style.css
 │   ├── script.js
 │   └── taste_of_anime.code-workspace
 └── README.md
