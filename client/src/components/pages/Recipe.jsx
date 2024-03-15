08350e9ac41665d44022ef9c9208143e7c960fd7
delafiado@gmail.com <delafiado@gmail.com>
2024-03-15 00:21:26 +0000

    wrapping up with the frontend

--- client/src/components/pages/Recipe.jsx
+++ client/src/components/pages/Recipe.jsx
@@ -3,7 +3,36 @@ import styled from 'styled-components';
 import { useParams } from 'react-router-dom';
 
 function Recipe() {
-    return <div>Recipe</div>;
+    let params = useParams();
+    const [details, setDetails] = useState({});
+
+    const fetchDetails = async () => {
+        const data = await fetch(`https://api.spoonacular.com/recipes/${params.name}/information?apiKey=${process.env.REACT_APP_API_KEY}`);
+        const detailData = await data.json();
+        setDetails(detailData);
+    };
+
+    useEffect(() => {
+        fetchDetails();
+    }, [params.name]);
+
+    return <div>{details.title}</div>;
 }
+
+const DetailWrapper = styled.div`
+    margin-top: 10rem;
+    margin-top: 5rem;
+    display: flex;
+    h2 {
+        margin-bottom: 2rem;
+    }
+    li {
+        font-size: 1.2rem;
+        line-height: 2.5rem;
+    }
+    ul {
+        margin-top: 2rem;
+    }
+`;
 
 export default Recipe;
