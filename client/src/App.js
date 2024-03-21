97d1b1285a00ef58bcaea844e73ad569a9dd7455
delafiado@gmail.com <delafiado@gmail.com>
2024-03-21 00:36:28 +0000

    made some changes

--- client/src/App.js
+++ client/src/App.js
@@ -3,11 +3,18 @@ import Pages from './components/pages/Pages';
 import Category from './components/Category';
 import { BrowserRouter } from 'react-router-dom';
 import Search from './components/Search';
+import { styled } from 'styled-components';
+import { Link } from 'react-router-dom';
+import { GiKnifeFork } from 'react-icons/gi';
 
 function App() {
     return (
         <div className="App">
             <BrowserRouter>
+                <Nav>
+                    <GiKnifeFork />
+                    <Logo to={'/'}>TOA</Logo>
+                </Nav>
                 <Search />
                 <Category />
                 <Pages />
@@ -15,5 +22,23 @@ function App() {
         </div>
     );
 }
+
+const Logo = styled(Link)`
+    text-decoration: none;
+    font-size: 400;
+    font-size: 1.5rem;
+    font-family: 'Lobster Two', cursive;
+`;
+
+const Nav = styled.div`
+    padding: 4rem 0;
+    display: flex;
+    justify-content: flex-start;
+    align-items: center;
+
+    svg {
+        font-size: 2rem;
+    }
+`;
 
 export default App;
